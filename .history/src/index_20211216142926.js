bb3517d755fd7d91369e6f7625b6bc4fcf49e736
gomgomigom <nodam89@naver.com>
2021-12-16 14:50:37 +0900

    Add simpleHabit: React hook practice

--- .history/src/index_20211216142926.js
+++ .history/src/index_20211216142926.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './app';
 import SimpleHabit from './simpleHabit';
 import '@fortawesome/fontawesome-free/js/all.js';
 
---
+++ .history/src/simpleHabit_20211216142605.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+const SimpleHabit = (props) => {
+  const [count, setCount] = useState(0);
+
+  return (
+    <li className='habit'>
+      <span className='habit-name'>Reading</span>
+      <span className='habit-count'>{this.state.count}</span>
+      <button
+        className='habit-button habit-increase'
+        onClick={this.handleIncrement}
+      >
+        <i className='fas fa-plus-square'></i>
+      </button>
+    </li>
+  );
+};
+
+export default SimpleHabit;
+
+class SimpleHabit extends Component {
+  handleIncrement = () => {
+    this.setState({ count: this.state.count + 1 });
+  };
+
+  render() {}
+}
