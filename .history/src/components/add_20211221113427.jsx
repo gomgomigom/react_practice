7fd09539cd73dba354c02555e29f187b0726c743
gomgomigom <nodam89@naver.com>
2021-12-21 11:38:43 +0900

    Modify add.jsx memo to PureComponent

---
+++ .history/src/components/add_20211221113427.jsx
@@ -0,0 +1,29 @@
+import React, { memo } from 'react';
+
+class Add extends PureComponent{
+  const formRef = React.createRef();
+  const inputRef = React.createRef();
+
+  const onSubmit = (event) => {
+    event.preventDefault();
+    const habitName = inputRef.current.value;
+    habitName && props.onAdd(habitName);
+    formRef.current.reset();
+    // inputRef.current.value = '';
+  };
+
+  return (
+    <form ref={formRef} onSubmit={onSubmit} className='input'>
+      <input
+        ref={inputRef}
+        type='text'
+        className='input box'
+        placeholder='input your habit'
+      />
+      <button className='input__btn'>Add</button>
+    </form>
+  );
+});
+}
+
+export default Add;
---
+++ .history/src/components/add_20211221113518.jsx
@@ -0,0 +1,30 @@
+import React, { memo } from 'react';
+
+class Add extends PureComponent{
+  const formRef = React.createRef();
+  const inputRef = React.createRef();
+
+  const onSubmit = (event) => {
+    event.preventDefault();
+    const habitName = inputRef.current.value;
+    habitName && props.onAdd(habitName);
+    formRef.current.reset();
+    // inputRef.current.value = '';
+  };
+
+  render(){
+    return (
+    <form ref={formRef} onSubmit={onSubmit} className='input'>
+      <input
+        ref={inputRef}
+        type='text'
+        className='input box'
+        placeholder='input your habit'
+      />
+      <button className='input__btn'>Add</button>
+    </form>
+  );
+});
+}
+
+export default Add;
